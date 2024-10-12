1f3c2c6ed5b473ec0f9440503637d16ac0be62fb
Yang <91305697+chungyu1108@users.noreply.github.com>
2024-10-11 21:16:47 -0700

    Time: 617 ms (40.05%), Space: 0B (100%) - LeetHub

---
+++ 1718-warehouse-manager/1718-warehouse-manager.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT   
+    w.name AS warehouse_name,  
+    SUM(w.units * p.Width * p.Length * p.Height) AS volume  
+FROM   
+    Warehouse w  
+JOIN   
+    Products p ON w.product_id = p.product_id  
+GROUP BY   
+    w.name;
